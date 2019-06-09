dafc19a26c59281afc5dd771c81a986e918afb28
andry-dev <peketribal2@gmail.com>
2019-06-09 11:44:38 +0200

    Fixed conan and CMake idiocy

--- conanfile.py
+++ conanfile.py
@@ -1,33 +1,16 @@
 from conans import ConanFile, CMake, tools
 
-class TewiConan(ConanFile):
+class AslConan(ConanFile):
     name = "asl"
     version = "0.1"
     license = "MIT"
     url = "https://github.com/andry-dev/asl.git"
-    description = "An easy to use, extendable, game engine"
-    settings = "os", "compiler", "build_type", "arch"
-
-    options = {
-        "shared": [True, False],
-    }
-
-    default_options = {
-        "shared": False,
-    }
-
-    generators = "cmake"
+    description = "Another support library"
     exports_sources = "include/*"
-
-    def build(self):
-        cmake = CMake(self)
-        self.run("cmake .. %s" % (cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
-
+    no_copy_sources = True
 
     def package(self):
-        self.copy("*", dst="include", src="include")
+        self.copy("*")
 
-    def package_info(self):
-        self.cpp_info.libs = ["asl"]
-
+    def package_id(self):
+        self.info.header_only()
